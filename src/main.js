930deafb3142d21432ee87a83a9d28e9d954ada8
Harper Reed <harper@nata2.org>
2021-08-21 18:07:02 +0000

    initial tailwind setup for vue2

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 
 import titleMixin from '@/mixins/title';
 
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
-
 import router from '@/router';
 import { store } from '@/store';
 
@@ -12,6 +9,7 @@ import VueMasonry from 'vue-masonry-css';
 import { VuePlausible } from 'vue-plausible';
 import Moment from 'vue-moment';
 import App from './App.vue';
+import './index.css';
 
 Vue.use(VuePlausible);
 
@@ -22,8 +20,6 @@ Vue.mixin(titleMixin);
 
 Vue.config.productionTip = false;
 Vue.$plausible.enableAutoPageviews();
-
-Vue.use(Buefy);
 
 new Vue({
   render: (h) => h(App),
