8660b25589e361711ed1f3d70d127a4669206b52
Harper Reed <harper@nata2.org>
2021-08-23 18:37:43 -0500

    cleaned up. no store. no masonry

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import Vue from 'vue';
 import titleMixin from '@/mixins/title';
 
 import router from '@/router';
-import { store } from '@/store';
 
-import VueMasonry from 'vue-masonry-css';
 import { VuePlausible } from 'vue-plausible';
 import Moment from 'vue-moment';
 import App from './App.vue';
@@ -15,7 +13,6 @@ Vue.use(VuePlausible);
 
 Vue.use(Moment);
 
-Vue.use(VueMasonry);
 Vue.mixin(titleMixin);
 
 Vue.config.productionTip = false;
@@ -24,5 +21,4 @@ Vue.$plausible.enableAutoPageviews();
 new Vue({
   render: (h) => h(App),
   router,
-  store,
 }).$mount('#app');
